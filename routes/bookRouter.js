2e599df60943a3ec299c3f822d437be5a4cbf46d
Dagimawi Mantefardo <dagimante101@gmail.com>
2022-08-08 21:54:37 +0300

    finished book routes

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -46,23 +46,95 @@ router.post("/", async (req, res) => {
     saveCover(book, req.body.cover);
     try{
         const newBook = await book.save();
-        // res.redirect(`/books/${newBook.id}`);
-        res.redirect("/books");
+        res.redirect(`/books/${newBook.id}`);
     }catch(e){
         renderNewBook(res, new Book(), true);
     }
 })
 
-async function renderNewBook(res, passedbook, hasError = false){
+router.get("/:id", async (req, res) => {
+    try{
+        const book = await Book.findById(req.params.id).populate("author").exec();
+        res.render("books/show", {book});
+    }catch{
+        res.redirect("/");
+    }
+});
+
+router.get("/:id/edit", async (req, res) => {
+    try{
+        const book = await Book.findById(req.params.id);
+        renderEditPage(res, book);
+    }catch{
+        res.redirect("/");
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    let book;
+    try{
+        book = await Book.findById(req.params.id);
+        book.title =  req.body.title;
+        book.author =  req.body.author;
+        book.publishDate =  new Date(req.body.publishDate);
+        book.pageCount =  req.body.pageCount;
+        book.description =  req.body.description;
+        if(req.body.cover != null && req.body.cover != ""){
+            saveCover(book, req.body.cover)
+        }
+        await book.save();
+        res.redirect(`/books/${book.id}`);
+    }catch{
+        if(book != null){
+            renderEditPage(res, book, true);
+        }else{
+            res.redirect("/");
+        }
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    let book;
+    try{
+        book = await Book.findById(req.params.id);
+        await book.remove();
+        res.redirect("/books");
+    }catch{
+        if(book != null){
+            res.render("books/show", {
+                book,
+                errorMessage: "Coudn't remove book"
+            })
+        }else{
+            res.redirect("/");
+        }
+    }
+});
+
+async function renderNewBook(res, book, hasError = false){
+    renderFormPage(res, book, "create", hasError);
+}
+
+async function renderEditPage(res, book, hasError = false){
+    renderFormPage(res, book, "edit", hasError);
+}
+
+async function renderFormPage(res, passedBook, file, hasError=false){
     try{
         const authors = await Author.find();
-        const book = passedbook;
+        const book = passedBook;
         const params = {
             authors,
             book
         }
-        if(hasError) params.errorMessage = "Coundn't create new book";
-        res.render("books/create", params);
+        if(hasError){
+            if(file == "edit"){
+                params.errorMessage = "Coundn't edit book";
+            }else{
+                params.errorMessage = "Coundn't create new book";
+            }
+        }
+        res.render(`books/${file}`, params);
     }catch{
         res.redirect("/books");
     }
--- server.js
+++ server.js
@@ -32,4 +32,4 @@ app.use("/books", booksRouter);
 
 app.listen(process.env.PORT || 3000);
 
-// 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
+// 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
